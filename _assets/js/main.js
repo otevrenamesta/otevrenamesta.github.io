9c71c7ee390e4098d90bee4eaa1c9a2339ec268d
Vaclav Klecanda <vencax77@gmail.com>
2015-02-11 20:01:34 +0100

    top-bar toggle menu icon, default template changes

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,4 +1,4 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
-$(document).foundation('topbar', 'reflow');
+// $(document).foundation();
+// $(document).foundation('topbar', 'reflow');
